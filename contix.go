c2b1a161d57b7e58d2a87cde5b9097594dd4136f
Zeuxis Lo <seekstudio@gmail.com>
2017-02-21 17:25:07 +0800

    custom console informations

--- contix.go
+++ contix.go
@@ -6,7 +6,18 @@ import (
     "github.com/codegangsta/cli"
 )
 
+const (
+    APP_VERSION = "0.1.0"
+)
+
 func main() {
     app := cli.NewApp()
+    app.Name = "Contix"
+    app.Usage = "A application console for contix"
+    app.Version = APP_VERSION
+    app.Commands = []cli.Command{
+
+    }
+    app.Flags = append(app.Flags, []cli.Flag{}...)
     app.Run(os.Args)
 }
