03e55104f6c7e12be29f766372579a8934577970
Zeuxis Lo <seekstudio@gmail.com>
2017-02-23 17:50:27 +0800

    add read proxy-fetch.txt function in proxy update command

--- commands/proxy/update.go
+++ commands/proxy/update.go
@@ -2,8 +2,14 @@ package proxy
 
 import (
     "fmt"
+    "os"
+    "bytes"
+    "io"
+    "bufio"
 
     "github.com/codegangsta/cli"
+
+    "github.com/zeuxisoo/go-contix/configs"
 )
 
 var CmdProxyUpdate = cli.Command{
@@ -16,7 +22,44 @@ var CmdProxyUpdate = cli.Command{
 }
 
 func proxyUpdate(cli *cli.Context) error {
-    fmt.Println("This is a proxy update command")
+    proxyList, err := readProxyFetchFile()
+    if err != nil {
+        return err
+    }
+
+    fmt.Println(proxyList)
 
     return nil
+}
+
+func readProxyFetchFile() ([]string, error) {
+    file, err := os.Open(configs.ProxyFetchFilePath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    buffer := bytes.NewBuffer(make([]byte,1024))
+
+    var lines []string
+    for {
+        line, prefix, err := reader.ReadLine()
+        if err != nil {
+            break
+        }
+
+        buffer.Write(line)
+
+        if !prefix {
+            lines = append(lines, buffer.String())
+            buffer.Reset()
+        }
+    }
+
+    if err == io.EOF {
+        return nil, err
+    }
+
+    return lines, nil
 }
---
+++ configs/default.go
@@ -0,0 +1,5 @@
+package configs
+
+const (
+    ProxyFetchFilePath = "data/proxy-fetch.txt"
+)
