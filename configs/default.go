a1bfd19dfbb26d3ec558f9c29bca6a16a92dd55c
Zeuxis Lo <seekstudio@gmail.com>
2017-03-03 15:04:16 +0800

    support application can run outside the program directory

--- configs/default.go
+++ configs/default.go
@@ -1,10 +1,60 @@
 package configs
 
-const (
-    ProxyFetchFilePath = "data/proxy-fetch.txt"
-    ProxyPoolFilePath  = "data/proxy-pool.txt"
+import (
+    "os"
+    "os/exec"
+    "path/filepath"
+    "strings"
 
-    CronTaskFilePath = "data/cron-task.yaml"
-
-    MailNotificationFilePath = "data/mail-notification.txt"
+    "github.com/zeuxisoo/go-contix/utils/log"
 )
+
+var (
+    ApplicationPath             string
+
+    ProxyFetchFilePath          string
+    ProxyPoolFilePath           string
+
+    CronTaskFilePath            string
+
+    MailNotificationFilePath    string
+)
+
+func init() {
+    var err error
+    if ApplicationPath, err = executePath(); err != nil {
+        log.Fatalf("Fail to get the application path, error = %v", err)
+    }
+
+    ApplicationPath = strings.Replace(ApplicationPath, "\\", "/", -1)
+
+    appDirectory, err := applicationDirectory()
+    if err != nil {
+        log.Fatalf("Fail to get the application directory, error = %v", err)
+    }
+
+    ProxyFetchFilePath = appDirectory + "/data/proxy-fetch.txt"
+    ProxyPoolFilePath  = appDirectory + "/data/proxy-pool.txt"
+
+    CronTaskFilePath = appDirectory + "/data/cron-task.yaml"
+
+    MailNotificationFilePath = appDirectory + "/data/mail-notification.txt"
+}
+
+func executePath() (string, error) {
+    file, err := exec.LookPath(os.Args[0])
+    if err != nil {
+        return "", err
+    }
+
+    return filepath.Abs(file)
+}
+
+func applicationDirectory() (string, error) {
+    i := strings.LastIndex(ApplicationPath, "/")
+    if i == -1 {
+        return ApplicationPath, nil
+    }
+
+    return ApplicationPath[:i], nil
+}
