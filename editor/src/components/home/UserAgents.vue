3aeda3f065880f942aa2c7f9ce3fc31aa98fe07f
Zeuxis Lo <seekstudio@gmail.com>
2017-03-15 16:20:45 +0800

    support create user agent in user agent tab

--- editor/src/components/home/UserAgents.vue
+++ editor/src/components/home/UserAgents.vue
@@ -5,7 +5,7 @@
                 <span class="tab-subject">Agents</span>
             </el-col>
             <el-col :span="21" class="text-right">
-                <el-button type="success" size="small" class="button-create" v-on:click="">
+                <el-button type="success" size="small" class="button-create" v-on:click="showCreateUserAgentForm = true">
                     <i class="el-icon-plus el-icon--left"></i>
                     Create
                 </el-button>
@@ -33,6 +33,22 @@
             </el-row>
             <div class="line"></div>
         </div>
+
+        <!--Create user agent dialog-->
+        <el-dialog title="Performance" v-model="showCreateUserAgentForm">
+            <el-form label-width="120px">
+                <el-form-item label="ID">
+                    <el-input type="text" v-model.trim="createUserAgentForm.userAgent.name"></el-input>
+                </el-form-item>
+                <el-form-item label="Proxy server">
+                    <el-input type="text" v-model.trim="createUserAgentForm.userAgent.agent"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button v-on:click="showCreateUserAgentForm = false">Cancel</el-button>
+                <el-button type="primary" v-on:click="createUserAgent">OK</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -51,11 +67,41 @@ export default {
         }
     },
 
+    data() {
+        return {
+            showCreateUserAgentForm: false,
+            createUserAgentForm: {
+                userAgent: {
+                    name: "",
+                    agent: ""
+                }
+            }
+        }
+    },
+
     methods: {
         deleteUserAgent(id) {
             this.configs.user_agents = this.configs.user_agents.filter((user_agnet, index) => {
                 return index != id
             })
+        },
+
+        createUserAgent() {
+            // Clone object by simple way without _.clone(obj, true)
+            const userAgent = JSON.parse(JSON.stringify(this.createUserAgentForm.userAgent))
+
+            this.configs.user_agents.push(userAgent)
+            this.resetCreateUserAgentForm()
+            this.showCreateUserAgentForm = false
+        },
+
+        resetCreateUserAgentForm() {
+            this.createUserAgentForm = {
+                userAgent: {
+                    name: "",
+                    agent: ""
+                }
+            }
         }
     }
 }
