7644d91695d293caa7aea908eac979e719099259
Zeuxis Lo <seekstudio@gmail.com>
2017-03-15 14:55:06 +0800

    support delete user agent in user agents tab

--- editor/src/components/home/UserAgents.vue
+++ editor/src/components/home/UserAgents.vue
@@ -4,16 +4,20 @@
         <div class="line"></div>
         <el-row :gutter="20">
             <el-col :span="3">Name</el-col>
-            <el-col :span="21">Agent</el-col>
+            <el-col :span="19">Agent</el-col>
+            <el-col :span="2" class="text-center">Action</el-col>
         </el-row>
         <div class="line"></div>
-        <div class="user_agent" v-for="user_agent in configs.user_agents">
+        <div class="user_agent" v-for="(user_agent, index) in configs.user_agents">
             <el-row :gutter="20">
                 <el-col :span="3">
                     <el-input type="text" v-model.trim="user_agent.name"></el-input>
                 </el-col>
-                <el-col :span="21">
+                <el-col :span="19">
                     <el-input type="text" v-model.trim="user_agent.agent"></el-input>
+                </el-col>
+                <el-col :span="2" class="text-center">
+                    <el-button type="danger" icon="delete" v-on:click="deleteUserAgent(index)"></el-button>
                 </el-col>
             </el-row>
             <div class="line"></div>
@@ -33,6 +37,14 @@ export default {
         configs: {
             type: Object,
             required: true
+        }
+    },
+
+    methods: {
+        deleteUserAgent(id) {
+            this.configs.user_agents = this.configs.user_agents.filter((user_agnet, index) => {
+                return index != id
+            })
         }
     }
 }
